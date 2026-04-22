bcc0fd38e2000136f5d487000913214f1b35013a
Nikelly Gabrielly Portela Figueira FIGUEIRA <u71831545136@acad.pp.ifms.edu.br>
2026-04-22 12:19:17 -0400

    Comecei a fazer o trabalho =)

---
+++ src/main/java/com/mycompany/sistemanikellyfigueirabtrab/SistemaNikellyFigueiraBtrab.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sistemanikellyfigueirabtrab;
+
+/**
+ *
+ * @author u71831545136
+ */
+public class SistemaNikellyFigueiraBtrab {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
